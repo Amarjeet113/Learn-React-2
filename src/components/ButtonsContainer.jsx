c9bc3eec343a9f04aa97ecc9cb93f48c6a3e4d29
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-10-05 23:31:26 +0530

    Update ButtonsContainer.jsx

--- src/components/ButtonsContainer.jsx
+++ src/components/ButtonsContainer.jsx
@@ -2,29 +2,34 @@ import styles from "./ButtonsContainer.module.css";
 
 const ButtonsContainer = ({ onButtonClick }) => {
   const buttonNames = [
-    "AC",
+    "AC", // Clear All
+    "DEL", // Delete (new)
     "1",
     "2",
     "+",
     "3",
     "4",
-    "-",
     "5",
+    "-",
     "6",
-    "*",
     "7",
     "8",
+    "*",
     "/",
     "=",
     "9",
     "0",
     ".",
+    "%", // Percentage (new)
+    "x²", // Square (new)
+    "√", // Square Root (new)
   ];
 
   return (
     <div className={styles.buttonsContainer}>
-      {buttonNames.map((buttonName) => (
+      {buttonNames.map((buttonName, index) => (
         <button
+          key={index}
           className={styles.button}
           onClick={() => onButtonClick(buttonName)}
         >
